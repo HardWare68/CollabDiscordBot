8abc37095a5f7003f1dffd738092d3eeadbe857a
HardWare <postalex11@icloud.com>
2021-03-26 14:03:59 +0000

    added comments

--- index.js
+++ index.js
@@ -10,12 +10,14 @@ client.on('ready', () => {
  });
 
  client.on('message', message => {
+   //if the messgae doesn't start with the prefix or if its from the bot, don't do anything
    if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-   const args = message.content.slice(prefix.length).trim().split(/ +/);
-   const command = args.shift().toLowerCase();
+   const args = message.content.slice(prefix.length).trim().split(/ +/); //any arguments the user sends
+   const command = args.shift().toLowerCase(); //the command the user sends
 
   switch(command){
+    //just says hello
     case "hello":
       message.channel.send("Hello!");
   } 
