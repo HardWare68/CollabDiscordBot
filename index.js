fb6807c0af7c94ec3a96cf8c3cd5f973d3264664
HardWare <postalex11@icloud.com>
2021-03-26 14:02:27 +0000

    added prefixes and stuff

--- index.js
+++ index.js
@@ -9,8 +9,16 @@ client.on('ready', () => {
  console.log(`Logged in as ${client.user.tag}!`);
  });
 
- client.on('message', msg => {
-   msg.channel.send("Heck");
+ client.on('message', message => {
+   if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+   const args = message.content.slice(prefix.length).trim().split(/ +/);
+   const command = args.shift().toLowerCase();
+
+  switch(command){
+    case "hello":
+      message.channel.send("Hello!");
+  } 
  });
 
 //log in to the bot
