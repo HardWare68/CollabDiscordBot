eb654492752796f42cf46de9ff0b1ce88ccbfa43
Blaisin <>
2021-04-07 13:32:27 +0000

    Updated a comment

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ client.on('ready', () => {
       message.channel.send("STOP POSTING ABOUT AMONG US! I'M TIRED OF SEEING IT! MY FRIENDS ON TIKTOK SEND ME MEMES, ON DISCORD IT'S FUCKING MEMES! I was in a server, right? and ALL OF THE CHANNELS were just among us stuff. I-I showed my champion underwear to my girlfriend and t-the logo I flipped it and I said 'hey babe, when the underwear is sus HAHA DING DING DING DING DING DING DING DI DI DING' I fucking looked at a trashcan and said 'THAT'S A BIT SUSSY' I looked at my penis I think of an astronauts helmet and I go 'PENIS? MORE LIKE PENSUS'");
       break;
     
-    //random number 1 to 100
+    //random number crap
     case "random":
       if(args[0] == undefined || args[1] == undefined) {
         message.channel.send(Math.floor(Math.random() * 100) + 1); 
